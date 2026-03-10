525b700edc272dcc40b292efa1fabf44f9ca07d3
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-10 14:08:56 -0500

    completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    length = string.length;
+    newStr = "";
+    for(let i = length-1; i >= 0; i--){
+        newStr+= string.substring(i,i+1);
+    }
 
+    return newStr;
 };
 
 // Do not edit below this line
