39c056221f87d62bba04336f13f6b1e92838da74
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-10 15:47:22 -0500

    finished

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celcius = (num-32) * (5/9);
+  return Number(celcius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahrenheith = (num * (9/5) +32);
+  fahrenheith.toFixed(1);
+  return Number(fahrenheith.toFixed(1));
 };
+
 
 // Do not edit below this line
 module.exports = {
